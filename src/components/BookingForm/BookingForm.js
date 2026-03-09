7e00ffd6eb00b54621762e64ae536058735b57a5
Dhaval Daraji <darjidhaval51@gmail.com>
2026-03-09 14:37:08 +0530

    Add BookingForm Component

--- src/components/BookingForm/BookingForm.js
+++ src/components/BookingForm/BookingForm.js
@@ -1,52 +1,72 @@
 import { useState } from 'react';
 import './BookingForm.styles.css';
 
-const BookingForm = () => {
-  const [availableDates, setAvailableDates] = useState('');
-  const [availableTimes, setAvailableTimes] = useState('17:00');
-  const [guests, setGuests] = useState('');
-  const [occasions, setOccasions] = useState('Birthday');
+const BookingForm = ({ availableTimes, dispatch }) => {
+  const { times } = availableTimes;
+
+  const [bookings, setBookings] = useState({
+    date: '',
+    time: '17:00',
+    guests: '',
+    occasion: 'Birthday',
+  });
 
   // SUBMIT HANDLER
   const handleSubmit = e => {
     e.preventDefault();
 
     console.log(`
-    Date: ${availableDates},
-    Time: ${availableTimes},
-    Number of Guests: ${guests},
-    Occasion: ${occasions}`);
+    Date: ${bookings.date},
+    Time: ${bookings.time},
+    Number of Guests: ${bookings.guests},
+    Occasion: ${bookings.occasion}`);
 
-    setAvailableDates('');
-    setAvailableTimes('17:00');
-    setGuests('');
-    setOccasions('Birthday');
+    setBookings({
+      date: '',
+      time: '17:00',
+      guests: '',
+      occasion: 'Birthday',
+    });
+  };
+
+  // HANDLING MULTIPLE INPUTS
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    // [e.target.name] = e.target.time || e.target.guests || e.target.occasion
+    setBookings({ ...bookings, [name]: value });
   };
 
   return (
     <section name='reservations' className='booking-form'>
       <form className='form-container' onSubmit={handleSubmit}>
+        {/* DATE */}
         <label htmlFor='res-date'>Choose date</label>
         <input
           type='date'
+          name='date'
           id='res-date'
-          value={availableDates}
-          onChange={e => setAvailableDates(e.target.value)}
+          value={bookings.date}
+          onChange={e => {
+            setBookings(prevState => ({ ...prevState, date: e.target.value }));
+            dispatch({ type: 'UPDATE_TIMES', date: e.target.value });
+          }}
         />
+
+        {/* TIME */}
         <label htmlFor='res-time'>Choose time</label>
         <select
           id='res-time'
-          value={availableTimes}
-          onChange={e => setAvailableTimes(e.target.value)}
-          multiple={false}
+          name='time'
+          value={bookings.time}
+          onChange={handleChange}
         >
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
+          {times.map(time => (
+            <option key={time}>{time}</option>
+          ))}
         </select>
+
+        {/* NUMBER OF GUESTS */}
         <label htmlFor='guests'>Number of guests</label>
         <input
           type='number'
@@ -54,21 +74,27 @@ const BookingForm = () => {
           min='1'
           max='10'
           id='guests'
-          value={guests}
-          onChange={e => setGuests(e.target.value)}
+          name='guests'
+          value={bookings.guests}
+          onChange={handleChange}
         />
+
+        {/* TYPE OF OCCASION */}
         <label htmlFor='occasion'>Occasion</label>
         <select
           id='occasion'
-          value={occasions}
-          onChange={e => setOccasions(e.target.value)}
-          multiple={false}
+          value={bookings.occasion}
+          name='occasion'
+          onChange={handleChange}
         >
           <option>Birthday</option>
           <option>Engagement</option>
           <option>Anniversary</option>
           <option>Wedding</option>
+          <option>Other</option>
         </select>
+
+        {/* SUBMIT */}
         <input type='submit' value='Make Your reservation' />
       </form>
     </section>
