82efe02abe16a8319ddd8dd7a774ed7f5f822bd3
Dhaval Daraji <darjidhaval51@gmail.com>
2026-03-09 12:35:06 +0530

    Rename Bookings component to BookingForm

---
+++ src/components/BookingForm/BookingForm.js
@@ -0,0 +1,11 @@
+import './BookingForm.styles.css';
+
+const BookingForm = () => {
+  return (
+    <section name='reservations' className='bookings'>
+      Bookings
+    </section>
+  );
+};
+
+export default BookingForm;
